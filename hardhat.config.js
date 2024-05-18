8d81935ae5a490678157f2efb8f9f0662e5cbcd7
firefly <firefly@LAPTOP-6I3M3P4V.localdomain>
2024-05-18 17:07:02 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,49 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomicfoundation/hardhat-verify")
+require("@nomiclabs/hardhat-ethers")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.24",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+    },
+
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
+}
